69176bb7c605ff6a31aba59318b81d9761ebe3df
seematomar01 <86758201+seematomar01@users.noreply.github.com>
2022-07-28 13:44:47 +0530

    Update app.py

--- cas-main/app.py
+++ cas-main/app.py
@@ -93,20 +93,20 @@ def gen_frames():  # generate frame by frame from camera
             print('facedis',facedis)
             
             matchIndex = np.argmin(facedis)
-            if np.argmin(facedis) < 1:
-                print('in')
+            #if np.argmin(facedis) < 1:
+            print('in')
                 
-                name = employeeName[matchIndex].upper()
-                #y1, x2, y2, x1 = faceloc
-                #y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-                #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                #cv2.rectangle(frame, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
-                #cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-                startX, startY, endX, endY = faceloc
-                cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+            name = employeeName[matchIndex].upper()
+            #y1, x2, y2, x1 = faceloc
+            #y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            #cv2.rectangle(frame, (x1, y2-25), (x2, y2), (0, 255, 0), cv2.FILLED)
+            #cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            startX, startY, endX, endY = faceloc
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
 
-                MarkAttendence(name)
-                print(name)
+            MarkAttendence(name)
+            print(name)
                 
 
         
